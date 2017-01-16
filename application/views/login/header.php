48285c0d83925063eb6ea32d7b851b2d921b4c11
Joel Andy Rojas Valencia <joel.a.rojas.v@gmail.com>
2017-01-16 02:06:21 -0400

    FrontEnd de la vista para el login y el registro de fieles

---
+++ application/views/login/header.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!--================================================================================
+	Item Name: Materialize - Material Design Admin Template
+	Version: 3.1
+	Author: GeeksLabs
+	Author URL: http://www.themeforest.net/user/geekslabs
+================================================================================ -->
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
+  <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
+  <title>Ingreso de Fieles</title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+  <!-- Favicons-->
+  <link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>assets/demo/images/favicon/apple-touch-icon-152x152.png">
+  <!-- For iPhone -->
+  <meta name="msapplication-TileColor" content="#00bcd4">
+  <meta name="msapplication-TileImage" content="<?php echo base_url(); ?>assets/demo/images/favicon/mstile-144x144.png">
+  <!-- For Windows Phone -->
+
+
+  <!-- CORE CSS-->
+
+  <link href="<?php echo base_url(); ?>assets/demo/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?php echo base_url(); ?>assets/demo/css/style.css" type="text/css" rel="stylesheet" media="screen,projection">
+    <!-- Custome CSS-->
+    <link href="<?php echo base_url(); ?>assets/demo/css/custom/custom.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?php echo base_url(); ?>assets/demo/css/layouts/page-center.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="<?php echo base_url(); ?>assets/demo/js/plugins/prism/prism.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?php echo base_url(); ?>assets/demo/js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+</head>
+
+<body class="teal lighten-2">
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
---
+++ application/views/template/footer.php
@@ -0,0 +1,41 @@
+
+    </div>
+    <!-- END WRAPPER -->
+  </div>
+  <!-- END MAIN -->
+<!-- START FOOTER -->
+       <footer class="page-footer">
+         <div class="footer-copyright">
+           <div class="container">
+             <span>Copyright © 2015 <a class="grey-text text-lighten-4" href="http://themeforest.net/user/geekslabs/portfolio?ref=geekslabs" target="_blank">GeeksLabs</a> All rights reserved.</span>
+             <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="http://geekslabs.com/">GeeksLabs</a></span>
+             </div>
+         </div>
+       </footer>
+   <!-- END FOOTER -->
+
+
+
+   <!-- ================================================
+   Scripts
+   ================================================ -->
+
+   <!-- jQuery Library -->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/plugins/jquery-1.11.2.min.js"></script>
+   <!--materialize js-->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/materialize.js"></script>
+   <!--prism-->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/plugins/prism/prism.js"></script>
+   <!--scrollbar-->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+   <!-- chartist -->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/plugins/chartist-js/chartist.min.js"></script>
+
+   <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/plugins.js"></script>
+   <!--custom-script.js - Add your own theme custom JS-->
+   <script type="text/javascript" src="<?php echo base_url(); ?>assets/demo/js/custom-script.js"></script>
+
+</body>
+
+</html>
