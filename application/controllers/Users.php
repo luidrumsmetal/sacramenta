729a1b9074febd874721c2dc6b67fadbc842d5d9
saulquiroz <saul85dt@gmail.com>
2017-01-25 08:54:15 -0400

    lista con paginacion de Sacerdote
    join two tables

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -24,6 +24,43 @@ class Users extends CI_Controller{
     $this->load->view('users/faithfulCreate',$data);
     $this->load->view('login/footer');
   }
+
+  function listSacerdote($offset = NULL)
+  {
+    $data['title'] = 'Lista de Sacerdotes';
+    $this->load->library('table');
+        $this->load->library('pagination');
+
+        $config['base_url'] = base_url().'users/listSacerdote';
+        $config['total_rows'] = $this->Users_model->count('sacerdote','persona');
+        $config['per_page'] = 10;
+        $config['next_link'] = 'Próxima';
+        $config['prev_link'] = 'Anterior';
+        $config['full_tag_open'] = '<div class="pagination alternate"><ul>';
+        $config['full_tag_close'] = '</ul></div>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li><a style="color: #2D335B"><b>';
+        $config['cur_tag_close'] = '</b></a></li>';
+        $config['prev_tag_open'] = '<li>';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['first_link'] = 'Primeira';
+        $config['last_link'] = 'Última';
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+
+    $this->pagination->initialize($config);
+    $data['results']= $this->Users_model->listGetSacerdote('persona, sacerdote, tiposacerdote',' idSacerdote, ci, nombre, apellido, tipoSacerdote','id = persona_id AND idTipoSacerdote = tipoSacerdote_id',$config['per_page'],$this->uri->segment(3));
+    $this->load->view('template/header',$data);
+    $this->load->view('users/listSacerdote',$data);
+    $this->load->view('template/footer');
+  }
+
+
   function faithfulRegister()
   {
     $tipoUsuario = 'fiel';
---
+++ application/views/users/listSacerdote.php
@@ -0,0 +1,130 @@
+
+<section id = "content">
+    <div class="section">
+      <div class="row">
+        <?php if($this->session->flashdata('error')) {?>
+            <div id="card-alert" class="card red">
+              <div class="card-content white-text">
+                <p><?php echo $this->session->flashdata('error') ?></p>
+              </div>
+              <button type="button" class="close red-text" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+        <?php } ?>
+        <?php if($this->session->flashdata('success')) {?>
+            <div id="card-alert" class="card green">
+              <div class="card-content white-text">
+                <p><?php echo $this->session->flashdata('success') ?></p>
+              </div>
+              <button type="button" class="close red-text" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+        <?php } ?>
+          <?php if (!$results){ ?>
+                        <h4 align="center">Lista de Sacerdotes</h4>
+          <div id="borderless-table">
+              <div class="row">
+                <div class="col s12 m8 l9">
+                  <table id="striped-table">
+                    <thead>
+                      <tr>
+                        <th data-field="id">ID</th>
+                        <th data-field="name">Nombre</th>
+                        <th data-field="price">Direccion</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>No existe parroquia</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+          </div>
+          <?php } else{ ?>
+
+            <div id="centered-table" class="col s10 m8 l9">
+                <div class="row">
+
+                  <div class="col s12 m12 l12">
+                    <h4 align="left">Lista de Sacerdotes</h4><hr><br>
+                    <table class="bordered striped">
+                      <thead>
+                        <tr>
+                          <th data-field="idSacerdote">#</th>
+                          <th data-field="id">CI</th>
+                          <th data-field="nombre">Nombre</th>
+                          <th data-field="apellido">Apellido</th>
+                          <th data-field="tipoSacerdote">Tipo de Sacerdote</th>
+                          <th data-field="opciones">Opciones</th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        <?php foreach ($results as $r) {
+                            echo '<tr>';
+                            echo '<td>'.$r->idSacerdote.'</td>';
+                            echo '<td>'.$r->ci.'</td>';
+                            echo '<td>'.$r->nombre.'</td>';
+                            echo '<td>'.$r->apellido.'</td>';
+                            echo '<td>'.$r->tipoSacerdote.'</td>';
+                            echo '<td>';
+            #if($this->permission->checkPermission($this->session->userdata('permissao'),'vCliente')){
+            #echo '<a href="'.base_url().'index.php/clientes/visualizar/'.$r->idClientes.'" style="margin-right: 1%" class="btn tip-top" title="Ver mais detalhes"><i class="icon-eye-open"></i></a>';
+            #}
+            #if($this->permission->checkPermission($this->session->userdata('permissao'),'eCliente')){
+                echo '<a href="'.base_url().'jurisdiccion/editParroquia/'.$r->idSacerdote.'" style="margin-right: 2%" class="btn waves-effect waves-light amber darken-4" title="Editar Cliente"><i class="mdi-editor-border-color"></i></a>';
+          #  }
+          #  if($this->permission->checkPermission($this->session->userdata('permissao'),'dCliente')){
+                echo '<a href="#modal1" cliente="'.$r->idSacerdote.'" style="margin-right: 1%" class="btn waves-effect waves-light btn modal-trigger red darken-4" title="Excluir Cliente"><i class="mdi-action-delete"></i></a>';
+          #  }
+
+
+            echo '</td>';
+                            echo '</tr>';
+                        }?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+            </div>
+            <?php } ?>
+            <?php echo $this->pagination->create_links();?>
+            <!--<div id="modal-excluir" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <form action="<?php echo base_url() ?>index.php/clientes/excluir" method="post" >
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                  <h5 id="myModalLabel">Excluir Cliente</h5>
+                </div>
+                <div class="modal-body">
+                  <input type="hidden" id="idCliente" name="id" value="" />
+                  <h5 style="text-align: center">¿Realmente desea eliminar este cliente y los datos asociados con él (OS, ventas, ingresos)?</h5>
+                </div>
+                <div class="modal-footer">
+                  <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
+                  <button class="btn btn-danger">Excluir</button>
+                </div>
+                </form>
+              </div>-->
+
+              <div id="modal1" class="modal">
+                <form action="<?php echo base_url(); ?>jurisdiccion/parroquiaDelete" method="post">
+                  <div class="modal-content">
+                    <h4 align="center">Eliminar Parroquia</h4>
+                      <input type="hidden" id="idParroquia" name="idParroquia" value="" />
+                      <h6 style="text-align: center">¿Realmente desea eliminar esta parroquia?</h6>
+
+                  </div>
+                  <div class="modal-footer orange">
+                    <a href="#" class="waves-effect waves-orange btn-flat modal-action modal-close" style="margin-right: 2%">Cancelar</a>
+                    <button class="waves-effect waves-red btn-flat modal-action modal-close">Eliminar</button>
+                  </div>
+                </form>
+              </div>
+      </div>
+    </div>
+</section>
