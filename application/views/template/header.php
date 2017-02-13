af407af38b800d62215f01c64010ddb76eab80d2
LauraPereyra <laurapereyra2504@gmail.com>
2017-02-13 15:37:22 -0400

    prueba 2.0
    prueba

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -70,15 +70,7 @@
                         </div>
                       </li> 
                     </ul>
-                    <ul class="right hide-on-med-and-down">
 
-                        <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light notification-button" data-activates="notifications-dropdown"><i class="mdi-social-notifications"><small class="notification-badge">5</small></i>
-
-                        </a>
-                        </li>
-                        <li><a href="#" data-activates="chat-out" class="waves-effect waves-block waves-light chat-collapse"><i class="mdi-communication-chat"></i></a>
-                        </li>
-                    </ul>
                     <!-- translation-button -->
                     <!-- notifications-dropdown -->
 
