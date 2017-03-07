9ff81cb25ca0445073f1d196cfb3367e3734daea
LauraPereyra <laurapereyra2504@gmail.com>
2017-03-07 15:34:28 -0400

    vistas
    pagina principal

--- application/views/inicio/footer.php
+++ application/views/inicio/footer.php
@@ -10,7 +10,7 @@
 					<li><a href="#" class="fa fa-facebook wow fadeIn" data-wow-delay="0.3s"></a></li>
 					<li><a href="#" class="fa fa-twitter wow fadeIn" data-wow-delay="0.6s"></a></li>
 				</ul>
-				<p><font color="black">Copyright &copy; 2017 JLS</font></p>
+				<p><font color="black">Pastoral Digital Bolivia</font></p>
 
 			</div>
 		</div>
